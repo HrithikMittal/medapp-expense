44ff33e1261973ca8403dd96f78832eac766cf5a
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-04-21 16:09:40 +0530

    changed atlas url admin name

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb+srv://attendance-medapp:0wbEt1fjGZuj6sB7@cluster0-4dvih.mongodb.net/expenseApp?retryWrites=true", {
+mongoose.connect("mongodb+srv://medapp-atlas:0wbEt1fjGZuj6sB7@cluster0-4dvih.mongodb.net/expenseApp?retryWrites=true", {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useFindAndModify: false
