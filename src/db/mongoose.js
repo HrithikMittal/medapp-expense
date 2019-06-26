cf5277abd3682e644940602c6bdf32f059b61a35
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-06-26 19:12:06 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const remoteDB = "mongodb+srv://medapp-atlas:0wbEt1fjGZuj6sB7@cluster0-4dvih.mongodb.net/expense?retryWrites=true"
+const remoteDB = "mongodb+srv://medapp-atlas:0wbEt1fjGZuj6sB7@cluster0-4dvih.mongodb.net/expenseApp?retryWrites=true"
 const localDB = "mongodb://localhost:27017/expenseApp"
 mongoose.connect(remoteDB, {
 	useNewUrlParser: true,
